7637ca138ebf6af4e67c395c3b85d864b209ee7c
Hoàng Dương <hoangvanduong1359@gmail.com>
2025-05-12 17:07:29 +0700

    design user ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import HomePage from './page/HomePage';
+import HomePage from './page/Dashboard';
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import FacialAttendance from './page/FacialAttendance';
 import DeepFacePage from './page/DeepFacePage';
+import Dashboard from './page/Dashboard';
+import Attendance from './page/Attendance';
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
-          <Route path='/' element={<HomePage></HomePage>}></Route>
+          <Route path='/' element={<Dashboard></Dashboard>}></Route>
           <Route path='/FacialAttendance' element = {<FacialAttendance></FacialAttendance>} ></Route>
           <Route path= '/deepface' element = {<DeepFacePage></DeepFacePage>}></Route>
+          <Route path='/Attendance' element = {<Attendance></Attendance>}></Route>
         </Routes>
       </BrowserRouter>
   );
