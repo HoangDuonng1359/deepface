762dff87860d987cb721cc3adac846ada29d8711
lamducanh2005 <23020326@vnu.edu.vn>
2025-05-17 20:23:57 +0700

    commit

--- backend/api/AttendanceApi.py
+++ backend/api/AttendanceApi.py
@@ -14,31 +14,51 @@ async def create_attendance(attendance: AttendanceCreateRequestEntity):
     """
     Tạo ca điểm danh mới, và trả về thông tin ca điểm danh đã tạo
     """
-    attendance = AttendanceService.create_attendance(attendance)
-    return ResponseEntity(
-        success=True,
-        message="Tạo ca điểm danh thành công",
-        data=attendance
-    )
+    attendance_id = AttendanceService.create_attendance(attendance.course_id)
+
+    if attendance_id is not None:
+        return {
+            "success": True,
+            "message": "Tạo ca điểm danh thành công",
+            "data": attendance_id
+        }
+    
+    return {
+        "success": False,
+        "message": "Tạo ca điểm danh thất bại",
+        "data": None
+    }
 
 @router.get("/{attendance_id}")
 async def get_attendance(attendance_id: int):
     """
     Lấy thông tin ca điểm danh theo ID
     """
-    attendance = AttendanceService.get_attendance_by_id(attendance_id)
-    if attendance is None:
-        return ResponseEntity(
-            success=False,
-            message="Ca điểm danh không tồn tại",
-            data=None
-        )
-    
-    return ResponseEntity(
-        success=True,
-        message="Lấy thông tin ca điểm danh thành công",
-        data=attendance
-    )
+    return {
+        "attendance_id" : attendance_id,
+        "course_id" : "INT2008-2",
+        "course_name" : "Công nghệ phần mềm",
+        "teacher_name" : "Nguyễn Đức Anh",
+        "start" : "2023-10-01 08:00:00",
+        "end" : "2023-10-01 09:00:00",
+        "students" : [
+            {
+                "student_id" : "23020001",
+                "student_name" : "Nguyễn Văn A",
+                "status" : "Đúng giờ"
+            },
+            {
+                "student_id" : "23020002",
+                "student_name" : "Nguyễn Văn B",
+                "status" : "Muộn"
+            },
+            {
+                "student_id" : "23020003",
+                "student_name" : "Nguyễn Văn C",
+                "status" : "Vắng"
+            }
+        ]
+    }
 
 @router.put("/{attendance_id}")
 async def check_attendance(attendance_id: int):
