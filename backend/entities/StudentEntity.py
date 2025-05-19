e4a52060a99db6369960b1a50365f6bc03f10a3f
lamducanh2005 <23020326@vnu.edu.vn>
2025-05-19 14:14:46 +0700

    update student

--- backend/entities/StudentEntity.py
+++ backend/entities/StudentEntity.py
@@ -3,13 +3,10 @@ from pydantic import BaseModel, Field
 class StudentCreateRequestEntity(BaseModel):
     student_id: str = Field(..., description="Mã sinh viên", example="23020001")
     student_name: str = Field(..., description="Tên sinh viên", example="Nguyen Van A")
+    cohort : str = Field(..., description="Lớp khóa học", example="K68-IT1")
     images: list[str] = Field(..., description="Danh sách hình ảnh sinh viên", example=["base64_image_string_1", "base64_image_string_2"])
 
 class StudentUpdateRequestEntity(BaseModel):
     student_name: str = Field(..., description="Tên sinh viên", example="Nguyen Van B")
+    cohort : str = Field(..., description="Lớp khóa học", example="K68-IT1")
     images: list[str] = Field(..., description="Danh sách hình ảnh sinh viên", example=["base64_image_string_3", "base64_image_string_4"])
-
-class StudentImageEntity(BaseModel):
-    image_id: str = Field(..., description="Mã ảnh sinh viên", example="1")
-    student_id: str = Field(..., description="Mã sinh viên", example="23020001")
-    base64_image: str = Field(..., description="Hình ảnh sinh viên", example="base64_image_string")
