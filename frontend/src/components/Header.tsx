d13c1b70c008d11f4a6a0ec53e44a8cc99674826
Hoàng Dương <hoangvanduong1359@gmail.com>
2025-05-19 13:09:28 +0700

    thêm chức năng detect khuôn mặt, kết nối api

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -15,27 +15,15 @@ const Header: React.FC = () => {
 
   return (
     <AntHeader className="flex items-center justify-between bg-gradient-to-r from-blue-600 to-indigo-700 px-6">
-      <div 
-        className="flex items-center cursor-pointer" 
-        onClick={goToHome}
-      >
-        <div className="flex items-center justify-center w-10 h-10 rounded-lg bg-white mr-4">
-          <span className="text-blue-600 font-bold text-xl">A</span>
+      <div className="flex items-center cursor-pointer" onClick={goToHome}>
+        <div className="flex items-center justify-center w-10 h-10 rounded-full bg-white mr-4">
+          <img src="/logo_uet.webp" alt="Logo UET" className="w-full h-full object-contain" />
         </div>
-        <Title level={3} className="text-white m-0">
-          Appname
+
+        <Title level={3}  style={{ color: 'white', margin: 0 }}>
+          Trường Đại Học Công Nghệ
         </Title>
       </div>
-
-      <Space size={16}>
-        <Badge count={3} size="small">
-          <BellOutlined className="text-white text-xl cursor-pointer" />
-        </Badge>
-        <Avatar 
-          icon={<UserOutlined />} 
-          className="bg-blue-300 cursor-pointer"
-        />
-      </Space>
     </AntHeader>
   );
 };
